3743ec533d6afdc6150ad5f7cf537ef83674b402
rootnure <nur.diu.2791@gmail.com>
2023-07-28 23:50:42 +0600

    Variable and Five things to declare a variable

---
+++ variable.js
@@ -0,0 +1,17 @@
+// vary + able --> variable
+
+var price = 11;
+var age = 17;
+var temperature = 38;
+var myAge = 25;
+var moneyInPocket = 330;
+var heightInInch = 67.5;
+
+/**
+ * Five things to declare a variable
+ * 1. Declaration (var, let, const)
+ * 2. Unique Name (price)
+ * 3. Assignment Operator (=)
+ * 4. Value (13)
+ * 5. Semicolon (;) [to indicate the end of statement]
+ */
