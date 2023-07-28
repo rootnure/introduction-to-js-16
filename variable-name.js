97008ccd88c3eac98f26ac558c5df35960e2512b
rootnure <nur.diu.2791@gmail.com>
2023-07-29 04:22:53 +0600

    Variable naming convention

---
+++ variable-name.js
@@ -0,0 +1,27 @@
+// good variables
+var price = 29;
+
+// 1. Variable name can not be any keywords
+var false = 44;
+var return = false;
+var null = 0;
+
+// 2. Variable name can not have any white space
+var my home address = "Dhaka, Bangladesh";
+
+// 3. Variable name can not starts with number or special character except _ (underscore) but can ends with number or have _ (underscore)
+var 99club = 1656;
+var club99 = 6354;
+var name_ = "Mariam";
+var _age = 22;
+var full_name = "Manna Day";
+
+// 4. Naming a long variable
+// can not use dash
+var user-name = "Alpha33";
+
+var user_name = "Raj Bro";
+var usercurrenthomeaddress = "Basabo, Dhaka";    // not a proper way of naming a variable
+var user_current_home_address = "Basabo, Dhaka"; // snake case (underscore between words)
+var userCurrentHomeAddress = "Basabo, Dhaka";    // camel case (capitalized each words except first word): we will use this one
+var UserCurrentHomeAddress = "Basabo, Dhaka";    // pascal case (capitalized each words)
