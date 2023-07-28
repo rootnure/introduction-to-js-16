0a8dbc5a460b3dbd9b35aeb5e1b48d9c91ff468e
rootnure <nur.diu.2791@gmail.com>
2023-07-28 23:19:43 +0600

    Math knowledge needed for this course

---
+++ math.js
@@ -0,0 +1,20 @@
+/**
+ * 1. number, integer, odd ==> 55, 834, -55
+ * 2. integer: 2, 5
+ * 3. float: decimal: 4.1, 44.34, -23.87
+ * 4. operation: addition, subtraction, multiplication, division, remainder
+ * 5. percent
+ * 6. average
+ * 7. comparison: >, <, ==, !=, >=, <=
+ * 8. oikik: 2 kg apple, $10 per kg and 5 kg mango, $45 per kg: total cost?
+ * 9. 23, 34, 15, 634, 2, 77: max, min, average, sum
+ * 10. series: related operation: sum, multiplication etc.
+ * 
+ * :::: EXTRA ::::
+ * 1. Prime number
+ * 2. Permutation
+ * 3. Area (Daily Life Basic Calculation)
+ * 
+ * ::::ALWAYS::::
+ * If needed, have the mindset to find unknown operation i.e. LMC, LMD
+ * */ 
