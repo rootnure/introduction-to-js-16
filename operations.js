b42cb9afba7c25946e2bfe1fec284ce34f4d48cb
rootnure <nur.diu.2791@gmail.com>
2023-07-29 04:47:34 +0600

    Basic Mathematical Operation in JS

---
+++ operations.js
@@ -0,0 +1,31 @@
+var onionPrice = 20;
+var eggPrice = 10;
+
+// additions and subtraction
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+// console.log(totalPrice);
+// console.log(priceDifference);
+
+
+var orangePrice = 20;
+var quantity = 7;
+// multiplication
+var productCost = orangePrice * quantity;
+// console.log(productCost);
+
+// division
+var money = 500;
+var player = 10;
+var eachPlayerWillGet = money / player;
+// console.log(eachPlayerWillGet);
+
+// remainder
+var riceInKG = 44;
+var eachWillGetInKG = 5;
+var remainingForLastPerson = 41 % 5;
+// console.log(remainingForLastPerson);
