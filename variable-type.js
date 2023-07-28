71f4800c0df38b0fd16db518c5b24f632632b15a
rootnure <nur.diu.2791@gmail.com>
2023-07-29 03:38:44 +0600

    Variable types

---
+++ variable-type.js
@@ -0,0 +1,30 @@
+/* Numeric, String, Boolean */
+
+// Numeric
+var age = 22;
+var distanceInKM = 5;
+
+// String (Non numeric, unable to express using number)
+var fullName = "Md. Nure Alom";
+var location = 'Dhaka';
+var special = `Jhankar Mahbub`;
+
+/**
+ * Things to check declaring string
+ * 1. String must be inside quotation
+ * 2.1. Multiple type of quotation to use
+ * 2.2. Single quotation (' '), Double quotation (" ") and Backtick (` `) 
+ * 2.2.1. If string starts using single quotation, must ends using single quotation i.e. 'eat'
+ * 2.2.2. If string starts using double quotation, must ends using double quotation i.e. "sleep"
+ * 2.2.3. If string starts using backtick, must ends using backtick i.e. `code`
+*/
+
+// Boolean  (yes/no, true/false etc.)
+var isRaining = false;
+var isSingle = true;
+var isLearning = true;
+var isHardToLearn = true;
+
+/**
+ * No need for any type of quotation to use reserve words or keywords as value i.e. true, false
+ */
