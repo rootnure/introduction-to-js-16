c49f04cece610a6d34752c33f1e939edb0b5bccc
rootnure <nur.diu.2791@gmail.com>
2023-07-29 14:06:03 +0600

    Practice Problem 1 solved

---
+++ task1.js
@@ -0,0 +1,6 @@
+var momGiveTK = 1000;
+var perKGOrangeCost = 400;
+var perKGAppleCost = 300;
+var totalCost = perKGOrangeCost + perKGAppleCost;
+var shopkeeperWillReturn = momGiveTK - totalCost;
+console.log(shopkeeperWillReturn);
