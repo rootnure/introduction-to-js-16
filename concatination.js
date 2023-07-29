0fcc6321fb94d4a5940fa802aab9faf4adb20fb9
rootnure <nur.diu.2791@gmail.com>
2023-07-29 12:51:04 +0600

    String Concatination

---
+++ concatination.js
@@ -0,0 +1,12 @@
+var price1 = 55;
+var price2 = 13;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+var firstName = 'Jennie';
+var lastName = 'Wannie';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);  // Jennie Wannie
+
+
+var concat = 22 + '22';
+console.log(concat);    // 2222
