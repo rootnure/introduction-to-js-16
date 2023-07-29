ad72389a4494101fc4f4253e3622ccc42f733331
rootnure <nur.diu.2791@gmail.com>
2023-07-29 13:18:05 +0600

    Type conversion

---
+++ conversion.js
@@ -0,0 +1,14 @@
+var price1 = 31;
+var price2 = '10';
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+// convert string to number
+console.log(price2);
+var price1Number = parseInt(price2);
+console.log(price1Number);
+
+var gpa = '3.69';
+var gpaInt = parseInt(gpa);
+var gpaFloat = parseFloat(gpa);
+console.log(gpaInt, gpaFloat);
