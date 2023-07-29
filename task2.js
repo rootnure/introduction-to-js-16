a437a128a64ac9b5d5b00176d116941394dbfaf2
rootnure <nur.diu.2791@gmail.com>
2023-07-29 14:11:51 +0600

    Practice Problem 2 Solved

---
+++ task2.js
@@ -0,0 +1,11 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalMarks = math + biology + chemistry + physics + bangla;
+var totalSubjects = 5;
+var averageMarks = totalMarks / totalSubjects;
+averageMarks = averageMarks.toFixed(2);
+averageMarks = parseFloat(averageMarks);
+console.log(averageMarks);
